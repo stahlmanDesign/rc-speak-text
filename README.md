a66478af0991e51c3dac001f649674f9a919acc4
Justin Stahlman <jstahlman@mac.com>
2018-03-20 11:27:24 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# rc-speak-text
+Uses the Web Speech API built into most modern browsers to vocalize a text string.
+
+## Git repository
+- https://github.com/stahlmanDesign/rc-speak-text
+
+## Installation
+- `npm install --save sd-react-loading`
+
+## Usage
+```import SpeakText from 'rc-speak-text'```
+### Basic
+```jsx
+<SpeakText
+  text='Bonjour tout le monde'
+  lang='fr-CA'
+/>
+```
+Will speak « Bonjour tout le monde » in French
+
+
+### Customizing with props	
+```jsx
+<SpeakText
+  text='Hello world'
+  lang='en-US'
+/>
+```
+Will speak “Hello world” in English
+
+## Source
+
+```jsx
+TODO
+
+```
+
+
+## As an NPM module
+- Built according to this tutorial to allow publishing the ES6 React JSX code as an NPM module
+- https://medium.com/@BrodaNoel/how-to-create-a-react-component-and-publish-it-in-npm-668ad7d363ce
+	
---
+++ build/index.js
@@ -0,0 +1,156 @@
+module.exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports) {
+
+	import React from 'react'
+	import PropTypes from 'prop-types'
+
+	class SpeakText extends React.Component {
+	  constructor (props) {
+	    super(props)
+
+	    if ('speechSynthesis' in window) {
+	      this.speech = this.init()
+	    } else {
+	      console.warn('This browser does not support the Web Speech API speechSynthesis. More info: https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API')
+	    }
+
+	    this.state = {
+	      started: false,
+	      playing: false
+	    }
+	  }
+	  componentWillUnmount () {
+	    this.cancel()
+	  }
+	  componentWillReceiveProps ({ pause }) {
+	    if (pause && this.state.playing && this.state.started) {
+	      return this.pause()
+	    }
+
+	    if (!pause && !this.state.playing && this.state.started) {
+	      return this.resume()
+	    }
+	  }
+	  shouldComponentUpdate () {
+	    return false
+	  }
+	  componentDidMount () {
+	    const events = [
+	      { name: 'start', action: this.props.onStart },
+	      { name: 'error', action: this.props.onError },
+	      { name: 'pause', action: this.props.onPause },
+	      { name: 'resume', action: this.props.onResume }
+	    ]
+
+	    events.forEach(e => {
+	      this.speech.addEventListener(e.name, e.action)
+	    })
+
+	    this.speech.addEventListener('end', () => {
+	      this.setState({ started: false })
+	      this.props.onEnd()
+	    })
+
+	    if (this.props.play) {
+	      this.speak()
+	    }
+	  }
+	  init () {
+	    const defaults = {
+	      text: '',
+	      volume: 1,
+	      rate: 1,
+	      pitch: 1,
+	      lang: 'en-US'
+	    }
+
+	    const options = Object.assign({}, defaults, this.props)
+
+	    let speech = new SpeechSynthesisUtterance()
+
+	    speech.text = options.text
+	    speech.volume = options.volume
+	    speech.rate = options.rate
+	    speech.pitch = options.pitch
+	    speech.lang = options.lang
+
+	    return speech
+	  }
+	  render () {
+	    return null
+	  }
+	  speak () {
+	    window.speechSynthesis.speak(this.speech)
+	    this.setState({ started: true, playing: true })
+	  }
+	  cancel () {
+	    window.speechSynthesis.cancel()
+	    this.setState({ started: false, playing: false })
+	  }
+	  pause () {
+	    window.speechSynthesis.pause()
+	    this.setState({ playing: false })
+	  }
+	  resume () {
+	    window.speechSynthesis.resume()
+	    this.setState({ playing: true })
+	  }
+	}
+
+	SpeakText.propTypes = {
+	  style: PropTypes.object,
+	  className: PropTypes.string,
+	}
+	SpeakText.defaultProps = {
+	  style: {},
+	  className: '',
+	}
+	export default SpeakText
+
+
+/***/ }
+/******/ ]);
